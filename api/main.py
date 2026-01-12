f53cfcf69335fb0713fbe34bb936897b664add2c
chuukkhaing <chuukkhaing96@gmail.com>
2026-01-12 14:56:10 +0630

    update-docker

--- api/main.py
+++ api/main.py
@@ -144,7 +144,7 @@ def openai_edit_image(input_path: str, prompt: str, size: str="1024x1024") -> Im
     with open(input_path, "rb") as f:
         result = client.images.generate(
             model="gpt-image-1",
-            image=f,
+            # image=f,
             prompt=prompt,
             size=size
         )
